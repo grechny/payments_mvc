256b1c4aa4efc86189bb672b785b1ea91568543c
khudnitsky <khudnitsky@gmail.com>
2016-01-15 16:43:52 +0300

    added transactions

--- services/src/main/java/by/pvt/khudnitsky/payments/services/AbstractService.java
+++ services/src/main/java/by/pvt/khudnitsky/payments/services/AbstractService.java
@@ -5,5 +5,5 @@ import by.pvt.khudnitsky.payments.entities.Entity;
 /**
  * Copyright (c) 2016, Khudnitsky. All rights reserved.
  */
-public abstract class AbsractService<T extends Entity> implements IService<T>{
+public abstract class AbstractService<T extends Entity> implements IService<T>{
 }
