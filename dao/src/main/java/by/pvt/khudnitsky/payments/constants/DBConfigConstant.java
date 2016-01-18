73c29799471d069fa3134a221d313c7c6c580033
khudnitsky <khudnitsky@gmail.com>
2016-01-18 16:45:10 +0300

    added DBConfigManager

---
+++ dao/src/main/java/by/pvt/khudnitsky/payments/constants/DBConfigConstant.java
@@ -0,0 +1,10 @@
+package by.pvt.khudnitsky.payments.constants;
+
+/**
+ * Copyright (c) 2016, Khudnitsky. All rights reserved.
+ */
+public class DBConfigConstant {
+    public static final String DATABASE_SOURCE = "database";
+
+    private DBConfigConstant(){}
+}
