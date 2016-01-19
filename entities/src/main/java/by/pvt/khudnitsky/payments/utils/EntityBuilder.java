0157676ec377915b989035aa6dfaf337e05f26d1
khudnitsky <khudnitsky@gmail.com>
2016-01-19 16:05:37 +0300

    added javaDOC to entities

--- entities/src/main/java/by/pvt/khudnitsky/payments/utils/EntityBuilder.java
+++ entities/src/main/java/by/pvt/khudnitsky/payments/utils/EntityBuilder.java
@@ -1,3 +1,6 @@
+/**
+ * Copyright (c) 2016, Khudnitsky. All rights reserved.
+ */
 package by.pvt.khudnitsky.payments.utils;
 
 import by.pvt.khudnitsky.payments.entities.Account;
@@ -6,11 +9,22 @@ import by.pvt.khudnitsky.payments.entities.Operation;
 import by.pvt.khudnitsky.payments.entities.User;
 
 /**
- * Copyright (c) 2016, Khudnitsky. All rights reserved.
+ * Utility class for entities buildings
+ * @author khudnitsky
+ * @version 1.0
  */
+
 public class EntityBuilder {
     private EntityBuilder(){}
 
+    /**
+     * Creates account
+     * @param id - account's id
+     * @param currency - account's currency
+     * @param amount - amount of money
+     * @param status - account's status
+     * @return entity of <b>Account</b>
+     */
     public static Account buildAccount(int id, String currency, double amount, int status){
         Account account = new Account();
         account.setId(id);
@@ -20,6 +34,16 @@ public class EntityBuilder {
         return account;
     }
 
+    /**
+     * Creates operation
+     * @param id - operation's id
+     * @param userId - user's id
+     * @param accountId - account's id
+     * @param amount - the value of the operation
+     * @param description - operation's description
+     * @param date - operation's date
+     * @return entity of <b>Operation</b>
+     */
     public static Operation buildOperation(int id, int userId, int accountId, double amount, String description, String date){
         Operation operation = new Operation();
         operation.setId(id);
@@ -31,6 +55,17 @@ public class EntityBuilder {
         return operation;
     }
 
+    /**
+     * Creates user
+     * @param id - user's id
+     * @param firstName - user's first name
+     * @param lastName user's last name
+     * @param aid - account's id
+     * @param login - user's login
+     * @param password - user's password
+     * @param accessLevel - user's access level (0 - client, 1 - administrator)
+     * @return entity of <b>User</b>
+     */
     public static User buildUser(int id, String firstName, String lastName, int aid, String login, String password, int accessLevel){
         User user = new User();
         user.setId(id);
@@ -43,6 +78,11 @@ public class EntityBuilder {
         return user;
     }
 
+    /**
+     * Creates card
+     * NOT REALISED
+     * @return throw UnsupportedOperationException()
+     */
     public static Card buildCard(){
         throw new UnsupportedOperationException();
     }
